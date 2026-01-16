844f82dae1ad6ca7ad334bcb7f54643c9656afa0
Edder Matheus <eddermatheus0@gmail.com>
2026-01-15 23:15:29 -0300

    Update Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -1,21 +1,22 @@
 n1 = float(input("Digite o primeiro número: "))
 n2 = float(input("Digite o segundo número: "))
-
+operacao = input('Digite a operacao')
 match operacao:
-    case "1":
+    case "+":
         resultado = n1 + n2
-    case "2":
+    case "-":
         resultado = n1 - n2
-    case "3":
+    case "/":
         if n2 == 0:
             print("Erro: divisão por zero")
+            
         else:
             resultado = n1 / n2
-    case "4":
+    case "*":
         resultado = n1 * n2
     case _:
         print("Opção inválida")
         resultado = None
 
-if resultado is not None:
-    print(f"Resultado: {resultado:.2f}")
+
+print(f"Resultado: {resultado:.2f}")
